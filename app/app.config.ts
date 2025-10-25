bcb62e10c6939545cb42403e23692d0471dc69b1
nicc <cai663081@gmail.com>
2025-10-25 17:05:06 +0800

    合并远程main分支的更改，解决冲突

---
+++ app/app.config.ts
@@ -0,0 +1,129 @@
+import type { Nav, NavItem } from '~/types/nav'
+import blogConfig from '~~/blog.config'
+
+// 图标查询：https://yesicon.app/ph
+// 图标插件：https://marketplace.visualstudio.com/items?itemName=antfu.iconify
+
+// @keep-sorted
+export default defineAppConfig({
+	// 将 blog.config 中的配置项复制到 appConfig，方便调用
+	...blogConfig,
+
+	article: {
+		categories: <{ [category: string]: { icon: string, color?: string } }>{
+			经验分享: { icon: 'ph:mouse-bold', color: '#3af' },
+			杂谈: { icon: 'ph:chat-bold', color: '#3ba' },
+			生活: { icon: 'ph:shooting-star-bold', color: '#f77' },
+			代码: { icon: 'ph:code-bold', color: '#77f' },
+			未分类: { icon: 'ph:folder-dotted-bold' },
+		},
+		defaultCategoryIcon: 'ph:folder-bold',
+		/** 分类排序方式，键为排序字段，值为显示名称 */
+		order: {
+			date: '创建日期',
+			updated: '更新日期',
+			// title: '标题',
+		},
+	},
+
+	content: {
+		/** 代码块自动折叠触发行数 */
+		codeblockCollapsibleRows: 16,
+		/** 文章开头摘要 */
+		excerpt: {
+			animation: true,
+			caret: '_',
+		},
+	},
+
+	// @keep-sorted
+	footer: {
+		/** 页脚版权信息，支持 <br> 换行等 HTML 标签 */
+		copyright: `© ${new Date().getFullYear()} ${blogConfig.author.name}`,
+		/** 侧边栏底部图标导航 */
+		iconNav: [
+			{ icon: 'ph:house-bold', text: '个人主页', url: blogConfig.author.homepage },
+			// { icon: 'ri:qq-line', text: '交流群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd' },
+			// { icon: 'ph:github-logo-bold', text: 'GitHub: L33Z22L11', url: 'https://github.com/L33Z22L11' },
+			{ icon: 'ph:rss-simple-bold', text: 'Atom订阅', url: '/atom.xml' },
+			{ icon: 'ph:subway-bold', text: '开往', url: 'https://www.travellings.cn/go-by-clouds.html' },
+		] satisfies NavItem[],
+		/** 页脚站点地图 */
+		nav: [
+			{
+				title: '探索',
+				items: [
+					{ icon: 'ph:rss-simple-bold', text: 'Atom订阅', url: '/atom.xml' },
+					{ icon: 'ph:subway-bold', text: '开往', url: 'https://www.travellings.cn/' },
+				],
+			},
+			// {
+			// 	title: '社交',
+			// 	items: [
+			// 		{ icon: 'ph:github-logo-bold', text: 'L33Z22L11', url: 'https://github.com/L33Z22L11' },
+			// 		{ icon: 'ri:qq-line', text: '群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd' },
+			// 		{ icon: 'ph:envelope-simple-bold', text: blogConfig.author.email, url: `mailto:${blogConfig.author.email}` },
+			// 	],
+			// },
+			{
+				title: '信息',
+				items: [
+					{ icon: 'simple-icons:nuxtdotjs', text: 'Nuxt开源博客主题', url: 'https://github.com/L33Z22L11/blog-v3' },
+					{ icon: 'ph:swatches-bold', text: '主题和组件文档', url: '/theme' },
+					// { icon: 'ph:certificate-bold', text: '陕ICP备2025072742号-3', url: 'https://beian.miit.gov.cn/' },
+				],
+			},
+		] satisfies Nav,
+	},
+
+	/** 左侧栏顶部 Logo */
+	header: {
+		logo: 'https://img.acyaer.nyc.mn/file/sVYSz8m9.png',
+		/** 展示标题文本，否则展示纯 Logo */
+		showTitle: true,
+		subtitle: blogConfig.subtitle,
+		emojiTail: ['📄', '🦌', '🙌', '🐟', '🏖️'],
+	},
+
+	/** 左侧栏导航 */
+	nav: [
+		{
+			title: '',
+			items: [
+				{ icon: 'ph:files-bold', text: '文章', url: '/' },
+				{ icon: 'ph:link-bold', text: '友链', url: '/link' },
+				{ icon: 'ph:archive-bold', text: '归档', url: '/archive' },
+			],
+		},
+	] satisfies Nav,
+
+	pagination: {
+		perPage: 10,
+		/** 默认排序方式，需要是 this.article.order 中的键名 */
+		sortOrder: 'date' as const,
+		/** 允许（普通/预览/归档）文章列表正序，开启后排序方式左侧图标可切换顺序 */
+		allowAscending: false,
+	},
+
+	stats: {
+		/** 归档页面每年标题对应的年龄 */
+		birthYear: 2003,
+		/** blog-stats widget 的预置文本 */
+		wordCount: '约10万',
+	},
+
+	themes: {
+		light: {
+			icon: 'ph:sun-bold',
+			tip: '浅色模式',
+		},
+		system: {
+			icon: 'ph:monitor-bold',
+			tip: '跟随系统',
+		},
+		dark: {
+			icon: 'ph:moon-bold',
+			tip: '深色模式',
+		},
+	},
+})
